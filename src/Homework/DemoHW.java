dac359375fcd8014cb3d87af0f7106c3760da1e7
slas2722 <140902956+slas2722@users.noreply.github.com>
2023-07-30 09:00:45 -0400

    Update DemoHW.java

--- src/Homework/DemoHW.java
+++ src/Homework/DemoHW.java
@@ -5,6 +5,11 @@ public class DemoHW {
 	public static void main(String[] args) {
 		
 		System.out.println("My name is Steven....");
+
+	public static void getData() {
+			System.out.println("data is this"  );
+			
+		}	
 	}
 
 }
